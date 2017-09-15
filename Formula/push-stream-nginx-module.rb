235aab57d42ce56f4933b8e5c9d1edb5933ed253
Artem Leontiev <artdokxxx@gmail.com>
2017-09-15 16:40:58 +0300

    Update push-stream-nginx-module.rb

--- Formula/push-stream-nginx-module.rb
+++ Formula/push-stream-nginx-module.rb
@@ -1,12 +1,12 @@
-class PushStreamNginxModule < Formula
+class NginxPushStreamModule < Formula
   desc "pure stream http push technology"
   homepage "https://github.com/wandenberg/nginx-push-stream-module"
-  url "https://github.com/wandenberg/nginx-push-stream-module/archive/0.5.2.tar.gz"
-  sha256 "1d07f38acdb8194bd49344b0ba21de101070de9b8731d27a8d22e928850bc199"
+  url "https://github.com/wandenberg/nginx-push-stream-module/archive/0.4.1.tar.gz"
+  sha256 "553584f557a3faec73702550e7e1417cbc0021f4f98468cc83e61e9d94def5cc"
 
   bottle :unneeded
 
   def install
-    pkgshare.install Dir["*"]
+    (share+"push-stream-nginx-module").install Dir["*"]
   end
 end
